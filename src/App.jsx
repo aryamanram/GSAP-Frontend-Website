6891552e47cb0c5ca0eb7914d66b326a9c51d6f5
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-08 16:13:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className = "flex-center">
+        <div className = "flex-center h-[100vh]">
             <h1 className = "text-3xl text-indigo-300">Hello, GSAP!</h1>
         </div>
     )
