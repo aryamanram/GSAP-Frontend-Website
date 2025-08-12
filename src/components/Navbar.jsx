fbfe60360d5b7cb1d3b8dd25b07140bbd132a8e6
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-12 16:35:26 -0500

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import {navLinks} from "../../constants/index.js";
 import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Navbar = () => {
     useGSAP(() => {
-        const 
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo(
+            'nav',
+            {backgroundColor: 'transparent'},
+            {backgroundColor: '#00000050',
+                backgroundFilter: 'blur(10px)',
+                duration: 1,
+                ease: 'power1.inOut'
+            });
     })
 
     return (
