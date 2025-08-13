1742ea1eda541121865823df0c75bb436bdc53ca
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-13 18:18:01 -0500

    line animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,15 @@ const Hero = () => {
             ease: 'expo.out',
             stagger: 0.06,
         })
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        })
     }, []);
 
     return (
