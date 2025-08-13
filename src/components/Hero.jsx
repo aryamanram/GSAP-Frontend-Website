cea2c34f60d8c756543f378164d2c3dbdea56bc9
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-13 12:22:47 -0500

    Animate title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,21 @@
+import {useGSAP} from "@gsap/react";
+import {SplitText} from "gsap/all";
+import gsap from "gsap";
+
 const Hero = () => {
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', {type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+        })
+    }, []);
+
     return (
         <section id = "hero" className = "noisy">
             <h1 className = "title">MOJITO</h1>
@@ -15,7 +32,22 @@ const Hero = () => {
                 className = "right-leaf"
             />
 
-            
+            <div className = "body">
+                <div className = "content">
+                    <div className = "space-y-5 hidden md:block">
+                        <p>Cool. Crisp. Classic.</p>
+                        <p className = "subtitle">
+                            Sip the Spirit <br /> of Summer
+                        </p>
+                    </div>
+                    <div className = "view-cocktails">
+                        <p className = "subtitle">
+                            Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                        </p>
+                        <a href = '#cocktails'>View Cocktails</a>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
