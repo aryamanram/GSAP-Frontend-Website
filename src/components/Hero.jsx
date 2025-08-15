d7eebf4d22e394e0d4a3874064e404bd1d552bfe
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-15 14:35:21 -0500

    reformatting hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,38 +37,40 @@ const Hero = () => {
     }, []);
 
     return (
-        <section id = "hero" className = "noisy">
-            <h1 className = "title">MOJITO</h1>
+        <>
+            <section id = "hero" className = "noisy">
+                <h1 className = "title">MOJITO</h1>
 
-            <img
+                <img
                 src = "/images/hero-left-leaf.png"
                 alt = "left-leaf"
                 className = "left-leaf"
-            />
+                />
 
-            <img
+                <img
                 src = "/images/hero-right-leaf.png"
                 alt = "right-leaf"
                 className = "right-leaf"
-            />
+                />
 
-            <div className = "body">
-                <div className = "content">
-                    <div className = "space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic.</p>
-                        <p className = "subtitle">
-                            Sip the Spirit <br /> of Summer
-                        </p>
-                    </div>
-                    <div className = "view-cocktails">
-                        <p className = "subtitle">
-                            Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
-                        </p>
-                        <a href = '#cocktails'>View Cocktails</a>
+                <div className = "body">
+                    <div className = "content">
+                        <div className = "space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className = "subtitle">
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
+                        <div className = "view-cocktails">
+                            <p className = "subtitle">
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                            </p>
+                            <a href = '#cocktails'>View Cocktails</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 export default Hero
