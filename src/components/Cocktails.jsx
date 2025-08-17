4336fff8c200ee7c3c4a36c7dcdb446ac941603d
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-08-17 11:47:40 -0500

    cocktails section comlete

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import {cocktailLists, mockTailLists} from "../../constants/index.js";
 
 const Cocktails = () => {
     return (
-        <div>Cocktails</div>
+        <section id = "cocktails" className = "noisy">
+            <img src = "/images/cocktail-left-leaf.png" alt = "left-leaf" id = "c-left-leaf"/>
+            <img src = "/images/cocktail-right-leaf.png" alt = "right-leaf" id = "c-right-leaf"/>
+
+            <div className="list">
+                <div className="popular">
+                    <h2>Most popular cocktails:</h2>
+
+                    <ul>
+                        {cocktailLists.map(({ name, country, detail, price }) => (
+                            <li key={name}>
+                                <div className="md:me-28">
+                                    <h3>{name}</h3>
+                                    <p>{country} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="loved">
+                    <h2>Most loved mocktails:</h2>
+
+                    <ul>
+                        {mockTailLists.map(({ name, country, detail, price }) => (
+                            <li key={name}>
+                                <div className="me-28">
+                                    <h3>{name}</h3>
+                                    <p>{country} | {detail}</p>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Cocktails
